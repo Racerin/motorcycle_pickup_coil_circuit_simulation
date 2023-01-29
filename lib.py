daa9c64e8e3d5373903a2430554e760f8f37a676
Darnell Baird <drsbaird@yahoo.com>
2023-01-28 21:44:03 -0400

    feats: Added alot. - raw spice / netlist functions - created the rectifier circuit - to be tested - fixed the parallel subcircuit issue (the name when added must be the same as name of sub circuit) - Add raw spice / netlist script/text file for input

--- lib.py
+++ lib.py
@@ -1,34 +1,62 @@
-import PySpice
+import os
+import math
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import PySpice.Logging.Logging as Logging
+from PySpice.Unit import *
 from PySpice.Spice.Netlist import Circuit, SubCircuitFactory
 from PySpice.Probe.WaveForm import OperatingPoint
-from PySpice.Unit import *
-import PySpice.Logging.Logging as Logging
+from PySpice.Doc.ExampleTools import find_libraries
+from PySpice.Spice.Library import SpiceLibrary
+from PySpice.Physics.SemiConductor import ShockleyDiode
 
 
 logger = Logging.setup_logging()
+libraries_path = find_libraries()
+spice_library = SpiceLibrary(libraries_path)
 
 
 __all__ = [
     'circuit1',
     'what_is_unit',
     'parallel_resistor_circuit',
+    'raw_spice_circuit',
+    'rectifier_circuit',
 ]
 
 
 class ParallelResistors(SubCircuitFactory):
     NAME = "ParallelResistors"
-    # __name__ = "ParallelResistors"
-    # __nodes__ = ('n1', 'n2',)
     NODES = ('n1', 'n2',)
 
-    def __init__(self, *r_units, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, name:str=NAME, *r_units, **kwargs):
+        # Add name
+        self.NAME = name
+        super().__init__(*[], **kwargs)
         for i, r_unit in enumerate(r_units):
             if isinstance(r_unit, type(1@u_Ohm)):
                 self.R(i, self.NODES[0], self.NODES[1], r_unit)
-                # self.R(1, 'n1', 'n2', 1@u_kOhm)
             else:
                 raise TypeError("r_unit {} is not a circuit value.".format(r_unit))
+
+class Rectifier(SubCircuitFactory):
+
+    NAME = 'Rectifier'
+    NODES = (
+        'input_1', 'input_2',
+        'output_1', 'output_2',
+    )
+
+    def __init__(self, diode_model):
+        super().__init__()
+
+        self.X('D1', diode_model, 'input_1', 'output_1')
+        self.X('D2', diode_model, 'output_2', 'input_2')
+        self.X('D3', diode_model, 'output_2', 'input_1')
+        self.X('D4', diode_model, 'input_2', 'output_1')
 
 
 def what_is_unit():
@@ -80,16 +108,43 @@ def circuit1():
     print_nodes(analysis)
 
 def parallel_resistor_circuit():
-    circuit = Circuit("Parallel Resistors")
+    circuit = Circuit("ParallelResistors")
 
     parallel_resistors = ParallelResistors(2@u_kOhm, 4@u_kOhm, 500@u_Ohm)
     circuit.subcircuit(parallel_resistors)
+    circuit.X('1', parallel_resistors.NAME, 1, circuit.gnd)
     circuit.V(1, 1, circuit.gnd, 5@u_V)
-    circuit.X('2', 'parallel_resistors', 1, circuit.gnd)
+    print(circuit)
 
     simulator = circuit.simulator(temperature=25, nominal_temperature=25)
     analysis = simulator.operating_point()
 
+    print_nodes(analysis)
+
+def raw_spice_circuit():
+    """ 
+    Use raw spice text to create circuit.
+    Reads 'Netlist' text file under 'assets'.
+      """
+    text = "".join(open(os.path.join(os.getcwd(),"assets/raw_spice.txt")).readlines())
+    # print(text)
+
+    circuit = Circuit("Raw Spice")
+    circuit.raw_spice = text
+    # circuit.R(2, 'out', 0, raw_spice='1k')
+
+    simulator = circuit.simulator(temperature=25, nominal_temperature=25)
+    analysis = simulator.operating_point()
+    print_nodes(analysis)
+
     print(circuit)
 
-    print_nodes(analysis)
+
+def rectifier_circuit():
+    circuit = Circuit("Rectifier")
+
+    diode = spice_library['1N4148']
+    print("These are the components in the spice library.")
+    for k,v in spice_library.items():
+        print("{}|{}".format(k,v))
+    # circuit.include(diode)
--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from lib import *
 
 @click.command()
 def cli():
-    click.echo("Hi!")
+    # click.echo("Hi!")
     # what_is_unit()
     # circuit1()
+    # raw_spice_circuit()
     parallel_resistor_circuit()
-    pass
+    # rectifier_circuit()
 
 if __name__ == "__main__":
     cli()
